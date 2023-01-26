640359fc1a351b83996e52f60862064fe7290bee
axvg <91773368+axvg@users.noreply.github.com>
2023-01-25 20:07:36 -0500

    :rocket:

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,25 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
+// type Data = {
+//   name: string
+// }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+import { NextRequest, NextResponse } from "next/server";
+
+export const config = {
+  runtime: "edge",
+};
+
+export default (req: NextRequest) => {
+  return NextResponse.json({
+    message: `Hello world ${req.url}`,
+  });
+};
